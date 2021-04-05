ca9d92a526c1e24643396d32b23a7bb2dcd58087
badallogical <badallearn@gmail.com>
2021-04-05 22:21:44 +0530

    configuring sign up

--- app/src/main/java/com/passion/attendancesheet/LoginActivity.java
+++ app/src/main/java/com/passion/attendancesheet/LoginActivity.java
@@ -12,13 +12,29 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if( currentUser != null ){
+            // already signed in
+        }
+    }
 }
--- app/src/main/java/com/passion/attendancesheet/fragments/Sign_up.java
+++ app/src/main/java/com/passion/attendancesheet/fragments/Sign_up.java
@@ -1,28 +1,44 @@
 package com.passion.attendancesheet.fragments;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.passion.attendancesheet.R;
 import com.passion.attendancesheet.databinding.FragmentSignUpBinding;
 import com.passion.attendancesheet.dataclasses.ClassRepresentative;
 import com.passion.attendancesheet.dataclasses.CourseF;
 import com.passion.attendancesheet.dataclasses.StudentF;
 
 import java.util.ArrayList;
+import java.util.concurrent.Executor;
 
 public class Sign_up extends Fragment {
 
     private FragmentSignUpBinding binding;
 
     private FirebaseDatabase database;
+
+    private FirebaseAuth mAuth;
+
+    private Context context;
+
 
     public Sign_up() {
         // Required empty public constructor
@@ -32,9 +48,14 @@ public class Sign_up extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        context = getContext();
+
         // Realtime Database
         database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("courses");
+
+        // Firebase Authentication
+        mAuth = FirebaseAuth.getInstance();
 
 
         ArrayList<CourseF> courses;
@@ -88,12 +109,72 @@ public class Sign_up extends Fragment {
                              Bundle savedInstanceState) {
 
         binding = FragmentSignUpBinding.inflate( getLayoutInflater() );
-
         return  binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+
+        // submit action
+        binding.submit.setOnClickListener( new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                // read data
+                String email = binding.editEmail.getText().toString();
+                String passwd = binding.editPasswd.getText().toString();
+
+                if( email.trim().isEmpty() ){
+                    binding.editEmail.setError("Empty Field");
+                }
+                else if( passwd.trim().isEmpty()){
+                    binding.editPasswd.setError("Empty Field");
+                }
+                else{
+                    // create account
+                    signUp(email,passwd);
+                }
+            }
+        });
+
     }
+
+    public void createAccount( String email, String passwd ){
+        mAuth.createUserWithEmailAndPassword( email, passwd )
+                .addOnCompleteListener((Activity) context, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if( task.isSuccessful() ){
+                            FirebaseUser currentUser = mAuth.getCurrentUser();
+                            updateUI(currentUser);
+                        }
+                        else{
+                            Toast.makeText(context, "Authentication Failed", Toast.LENGTH_LONG).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    public void signUp( String email, String passwd ){
+        createAccount(email,passwd);
+    }
+
+
+    private void updateUI(FirebaseUser user ){
+        if( user != null ){
+            Toast.makeText(context, "User created Successfully", Toast.LENGTH_LONG).show();
+            getParentFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container_view, Sign_in.class, null)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit();
+        }
+        else{
+            Toast.makeText(context, "User Failed to Create", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
